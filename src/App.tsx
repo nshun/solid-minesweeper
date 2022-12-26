26b43cfac571dee342308c15119aad59a89d4e14
Shun Nishimura <n.shun1110@gmail.com>
2022-12-27 00:45:20 +0900

    Add logics

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,79 @@
-import type { Component } from 'solid-js';
-import { createSignal, createMemo, Index } from 'solid-js';
+import { Component, createEffect, For, createSignal, createMemo } from 'solid-js';
+import Cell, { GridCell } from './components/Cell';
+import shuffle from './libs/shuffle';
 
-const maxGridPixelWidth = 500;
-
-function clampGridSideLength(newSideLength: number): number {
-  return Math.min(Math.max(newSideLength, 0), 100);
-}
+const isLeftEndCell = (i: number, size: number) => i % size === 0;
+const isRightEndCell = (i: number, size: number) => (i + 1) % size === 0;
+const isTopEndCell = (i: number, size: number) => i - size < 0;
+const isBottomEndCell = (i: number, size: number) => i + size >= size ** 2;
 
 const App: Component = () => {
-  const [gridSideLength, setGridSideLength] = createSignal(10);
-  const gridTemplateString = createMemo(() => `repeat(${gridSideLength()}, ${maxGridPixelWidth / gridSideLength()}px)`);
+  const [gridSideLength, setGridSideLength] = createSignal(20);
+  const [bombRate, setBombRate] = createSignal(20);
+  const [cells, setCells] = createSignal<GridCell[]>();
+  const gridTemplateString = createMemo(() => `repeat(${gridSideLength()}, 1fr)`);
+
+  createEffect(() => {
+    console.log('aaa');
+    const size = gridSideLength();
+    const rate = bombRate();
+    const cellNums = size ** 2;
+    let cells: GridCell[] = Array.from({ length: cellNums }, (_, i) => {
+      const [isOpen, setIsOpen] = createSignal(false);
+      const [isLock, setIsLock] = createSignal(false);
+
+      return {
+        isBomb: i < cellNums * (rate / 100),
+        numberOfNearbyBombs: 0,
+        isOpen,
+        open: () => setIsOpen(true),
+        isLock,
+        toggleLock: () => setIsLock(!isLock),
+      };
+    });
+    cells = shuffle(cells);
+    cells = cells.map((cell, i) => {
+      if (cell.isBomb) {
+        if (!isLeftEndCell(i, size)) cells[i - 1].numberOfNearbyBombs++;
+        if (!isRightEndCell(i, size)) cells[i + 1].numberOfNearbyBombs++;
+        if (!isTopEndCell(i, size)) cells[i - size].numberOfNearbyBombs++;
+        if (!isBottomEndCell(i, size)) cells[i + size].numberOfNearbyBombs++;
+        if (!isRightEndCell(i, size) && !isTopEndCell(i, size)) cells[i - size + 1].numberOfNearbyBombs++;
+        if (!isRightEndCell(i, size) && !isBottomEndCell(i, size)) cells[i + size + 1].numberOfNearbyBombs++;
+        if (!isLeftEndCell(i, size) && !isTopEndCell(i, size)) cells[i - size - 1].numberOfNearbyBombs++;
+        if (!isLeftEndCell(i, size) && !isBottomEndCell(i, size)) cells[i + size - 1].numberOfNearbyBombs++;
+      }
+      return cell;
+    });
+    setCells(cells);
+  });
 
   return (
-    <>
-      <div>
-        <label>Grid Side Length: </label>
-        <input
-          type="number"
-          value={gridSideLength()}
-          onInput={(e) => setGridSideLength(clampGridSideLength(e.currentTarget.valueAsNumber))}
-        />
+    <div class="container mx-auto">
+      <div class="my-3 text-xl space-x-8">
+        <span>
+          <label>Game Side Length: </label>
+          <select value={gridSideLength()} onInput={(e) => setGridSideLength(Number(e.currentTarget.value))}>
+            <For each={[10, 20, 40, 60]}>{(num) => <option value={num}>{num}</option>}</For>
+          </select>
+        </span>
+        <span>
+          <label>Bomb Rate: </label>
+          <select value={bombRate()} onInput={(e) => setBombRate(Number(e.currentTarget.value))}>
+            <For each={[10, 15, 20, 25, 30]}>{(num) => <option value={num}>{num} %</option>}</For>
+          </select>
+        </span>
       </div>
 
       <div
-        style={{
-          display: 'grid',
-          'grid-template-rows': gridTemplateString(),
-          'grid-template-columns': gridTemplateString(),
-        }}
-        class="m-4 p-0.5 gap-0.5 bg-slate-700 w-fit"
+        style={{ 'grid-template-columns': gridTemplateString() }}
+        class="p-0.5 grid gap-0.5 auto-rows-fr bg-slate-700 w-auto max-w-3xl aspect-square"
       >
-        <Index each={new Array(gridSideLength() ** 2)} fallback={'Input a grid side length.'}>
-          {() => (
-            <div
-              class="cell bg-slate-500"
-              onClick={(event) => {
-                const el = event.currentTarget;
-                el.classList.add('bg-red-200');
-              }}
-            ></div>
-          )}
-        </Index>
+        <For each={cells()} fallback={'Input a grid side length.'}>
+          {(cell) => <Cell cell={cell} />}
+        </For>
       </div>
-    </>
+    </div>
   );
 };
 
---
+++ src/components/Cell.tsx
@@ -0,0 +1,49 @@
+import { Accessor, Component, Match, Show, Switch } from 'solid-js';
+
+export interface GridCell {
+  isBomb: boolean;
+  numberOfNearbyBombs: number;
+  isOpen: Accessor<boolean>;
+  open: () => {};
+  isLock: Accessor<boolean>;
+  toggleLock: () => {};
+}
+
+const cellBackground = (cell: GridCell) => {
+  if (cell.isOpen()) {
+    return cell.isBomb ? 'bg-pink-400' : 'bg-slate-400';
+  }
+  if (cell.isLock()) {
+    return 'bg-slate-600';
+  }
+  return 'bg-slate-500';
+};
+
+interface CellProps {
+  cell: GridCell;
+}
+
+const Cell: Component<CellProps> = ({ cell }) => {
+  return (
+    <div
+      class={`cell ${cellBackground(
+        cell
+      )} hover:outline outline-cyan-500 w-full h-full flex justify-center items-center`}
+      onClick={cell.open}
+      onContextMenu={(event) => {
+        cell.toggleLock();
+        event.preventDefault();
+      }}
+    >
+      <Switch>
+        <Match when={cell.isLock()}>○</Match>
+        <Match when={cell.isOpen() && cell.isBomb}>×</Match>
+        <Match when={cell.isOpen() && cell.numberOfNearbyBombs > 0}>
+          <span class="text-xl">{cell.numberOfNearbyBombs}</span>
+        </Match>
+      </Switch>
+    </div>
+  );
+};
+
+export default Cell;
